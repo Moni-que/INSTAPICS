032a09d7c6ef0e05e48dbab793ccdb70e6125542
moni-que <monicahjustus@gmail.com>
2022-06-08 11:56:33 +0300

    create a like image path

--- insta/urls.py
+++ insta/urls.py
@@ -9,6 +9,7 @@ urlpatterns=[
     re_path(r'^signin',views.signin,name='signin'),
     re_path(r'^logout',views.logout,name='logout'),
     re_path(r'^upload',views.upload,name='upload'),
+    re_path(r'^like-post',views.like_post,name='like-post'),
     re_path(r'^profile',views.profile,name='profile'),
 
 ]
