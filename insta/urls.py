65dd8b16228ee7c5d6ad6398185e09b1259591ae
moni-que <monicahjustus@gmail.com>
2022-06-08 20:08:33 +0300

    make signin the first page

--- insta/urls.py
+++ insta/urls.py
@@ -4,9 +4,9 @@ from django.conf.urls.static import static
 from . import views
 
 urlpatterns=[
-    re_path(r'^$',views.home,name='home'),
+    re_path(r'^home',views.home,name='home'),
     re_path(r'^signup',views.signup,name='signup'),
-    re_path(r'^signin',views.signin,name='signin'),
+    re_path(r'^$',views.signin,name='signin'),
     re_path(r'^logout',views.logout,name='logout'),
     re_path(r'^upload',views.upload,name='upload'),
     re_path(r'^like-post',views.like_post,name='like-post'),
