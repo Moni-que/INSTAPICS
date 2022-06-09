5cb57dd2b459ecf980312fc91ebdb6e1f0c35395
moni-que <monicahjustus@gmail.com>
2022-06-09 08:46:43 +0300

    create a likepost model

--- insta/models.py
+++ insta/models.py
@@ -21,3 +21,9 @@ class Image(models.Model):
 
     def __str__(self):
         return self.image_caption
+
+class LikePost(models.Model):
+    post_id = models.CharField(max_length=500)
+    username = models.CharField(max_length=100)
+    def __str__(self):
+        return self.username
